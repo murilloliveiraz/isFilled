a1ef841658523ae483447187c0cab5ed25971c0e
murillo <113298979+murilloliveiraz@users.noreply.github.com>
2023-03-28 09:34:50 -0300

    Add files via upload

---
+++ isfilled.java
@@ -0,0 +1,10 @@
+public class isfilled {
+    public static void main (String [] args){
+     boolean isFilled = false;
+     if (isFilled == true) {
+        System.out.println("is shipping");
+     } else {
+        System.out.println("Order not ready");
+     }
+    }
+}
